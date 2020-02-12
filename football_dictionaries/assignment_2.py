98a1d69a62a00fb75c704a641217a19fd32940b2
djmccallister09 <60155683+djmccallister09@users.noreply.github.com>
2020-02-12 08:14:22 -0500

    Update assignment_2.py

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,3 +1,20 @@
+def players_as_dictionaries(squads_list):
+    players_list = []
+    for player in squads_list:
+        players_dict = {
+        'caps' : player[4],
+        'club': player[5],
+        'club_country': player[7],
+        'country': player[6],
+        'date_of_birth': player[3],
+        'name': player[2],
+        'number' : player[0],
+        'position': player[1],
+        'year': player[8]
+        }
+        players_list.append(players_dict)
+    return players_list
+
 def players_by_position(squads_list):
     squad = players_as_dictionaries(squad_list)
     by_position = {}
